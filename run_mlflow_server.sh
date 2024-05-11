e9598fdf0d1d7de5b57b1d2e6f8a8c1a6e1796ba
superVo1d <supervoid18@gmail.com>
2024-05-11 09:16:56 +0000

    Логирование моделей

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -4,6 +4,5 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_BUCKET_NAME=$S3_BUCKET_NAME
 
 mlflow server \
-  --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
-    --default-artifact-root s3://$AWS_BUCKET_NAME \
-    --no-serve-artifacts 
+  --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
+    --default-artifact-root s3://$AWS_BUCKET_NAME
